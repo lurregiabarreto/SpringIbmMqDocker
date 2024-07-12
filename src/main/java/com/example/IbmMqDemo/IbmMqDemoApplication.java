e48d3d47689f74e41187d767d0999b4c97877c28
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-07-12 08:27:22 -0300

    first commit

---
+++ src/main/java/com/example/IbmMqDemo/IbmMqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.IbmMqDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmMqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmMqDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IbmMqDemo/IbmMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IbmMqDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmMqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
